1c84d29f0966d804529c1c98c2523b401df14ee4
me <steven.kauthner@gmail.com>
2025-05-31 20:32:13 +0100

    Added -enable-bare-slash-regex flag to package

--- Package.swift
+++ Package.swift
@@ -22,7 +22,10 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Livestock"
+            name: "Livestock",
+            swiftSettings: [
+                .unsafeFlags(["-enable-bare-slash-regex"])
+            ]
         ),
         .testTarget(
             name: "LivestockTests",
