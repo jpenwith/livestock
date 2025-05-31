2c0b92526cee9538f50bd5aacd8570f6f57cb376
me <steven.kauthner@gmail.com>
2025-05-31 15:02:17 +0100

    Added @OptionalValidated

--- Sources/Livestock/Examples.swift
+++ Sources/Livestock/Examples.swift
@@ -9,4 +9,29 @@
 
 struct User {
     @Validated(.notEmpty, .lessThan(10), .between(0, 10)) var name: String = ""
+    
+    // Using the new ValidatedOptional property wrapper
+    @OptionalValidated(.notRequired, .notEmpty, .lessThan(10)) var nickname: String? = nil
+    
+    // Alternative way to define optional validation
+    @OptionalValidated(.notRequired, .notEmpty, .greaterThan(3)) var email: String? = nil
+    
+    // Example with custom validation for optional
+    @OptionalValidated var phoneNumber: String? = nil
+    
+    init(name: String, nickname: String? = nil, email: String? = nil, phoneNumber: String? = nil) {
+        self.name = name
+        self.nickname = nickname
+        self.email = email
+        self.phoneNumber = phoneNumber
+    }
 }
+
+//// Example of using optional validators
+//struct Profile {
+//    // Using .optional() extension to convert regular validators to optional validators
+//    @ValidatedOptional(AnyValidator<String>.notEmpty.optional()) var bio: String?
+//    
+//    // Requiring a non-nil value for an optional field
+//    @ValidatedOptional(AnyValidator<String>.notEmpty.optional(allowNil: false)) var username: String?
+//}
--- Sources/Livestock/Validated.swift
+++ Sources/Livestock/Validated.swift
@@ -6,11 +6,41 @@
 //
 
 @propertyWrapper struct Validated<Value> {
-    let validators: [AnyValidator<Value>]
     var wrappedValue: Value
+    let validators: [AnyValidator<Value>]
 
     init(wrappedValue: Value, _ validators: AnyValidator<Value>...) {
         self.validators = validators
         self.wrappedValue = wrappedValue
     }
 }
+
+// Specialized version for optional values
+@propertyWrapper struct OptionalValidated<Value> {
+    var wrappedValue: Value?
+    let required: Required
+    let validators: [AnyValidator<Value>]
+
+    init(wrappedValue: Value? = nil, _ required: Required = .notRequired, _ validators: AnyValidator<Value>...) {
+        self.wrappedValue = wrappedValue
+        self.required = required
+        self.validators = validators
+    }
+    
+    enum Required {
+        case required
+        case notRequired
+    }
+}
+
+// Type alias to make usage cleaner
+//typealias Validated_Optional<Value> = ValidatedOptional<Value>
+//
+//// Extension to allow using normal validators with optional values
+//extension AnyValidator {
+//    func optional(allowNil: Bool = true) -> AnyValidator<Value?> {
+//        .init(OptionalValidator<Value>(allowNil: allowNil, message: self.errorMessage) { value in
+//            self.validate(value)
+//        })
+//    }
+//}
