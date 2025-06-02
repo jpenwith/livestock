28cacfad01e207622fea3cf4614fb2d6bb475a0b
me <steven.kauthner@gmail.com>
2025-06-02 05:40:04 +0100

    feat: add composite Multiple validator to combine multiple validators in sequence

--- Sources/Livestock/Validators.swift
+++ Sources/Livestock/Validators.swift
@@ -8,3 +8,23 @@ import Foundation
 
 
 public enum Validators {}
+
+extension Validators {
+    public struct Multiple<Value>: Validator {
+        public let validators: [AnyValidator<Value>]
+
+        public init(validators: [AnyValidator<Value>]) {
+            self.validators = validators
+        }
+
+        public func validate(_ value: Value) throws(ValidationError) {
+            for validator in validators {
+                try validator.validate(value)
+            }
+        }
+    }
+}
+
+extension AnyValidator {
+    public static func multiple(_ validators: [AnyValidator<Value>]) -> Self { .init(Validators.Multiple(validators: validators)) }
+}
